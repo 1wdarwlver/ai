221c2873132a3ca71af4f8e25a553eb11abab7f2
1. <A111182133@nkust.edu.tw>
2026-04-30 12:17:51 +0800

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -6,7 +6,7 @@ import {defineConfig, loadEnv} from 'vite';
 export default defineConfig(({mode}) => {
   const env = loadEnv(mode, '.', '');
   return {
-    base:'/EK4BP6GJ4JO4DJ-RU0-/'
+    base:'/ai/'
     plugins: [react(), tailwindcss()],
     define: {
       'process.env.GEMINI_API_KEY': JSON.stringify(env.GEMINI_API_KEY),
